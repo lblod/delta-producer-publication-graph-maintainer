c2201591338de453edd2e2a3c3a0bc70a46b7684
Felix Ruiz de Arcaute <ruiz.felixruiz@gmail.com>
2021-05-11 02:10:29 +0200

    update main

--- jobs-processing/main.js
+++ jobs-processing/main.js
@@ -13,21 +13,29 @@ import { INITIAL_CACHE_SYNC_JOB_OPERATION,
 import { loadTask } from '../lib/task';
 import { runInitialSyncTask } from './pipeline-initial-sync';
 import { runHealingTask } from './pipeline-healing';
-import { parseResult } from '../lib/utils';
+import { parseResult, storeError } from '../lib/utils';
 
 export async function executeScheduledTask(){
-  //TODO: extra checks are required to make sure the system remains in consistent state
-  //so, e.g. only one scheduled task for jobs of interest, only one job of intersest a time etc..
-  const syncTaskUri = await getTaskUri(INITIAL_CACHE_SYNC_JOB_OPERATION, INITIAL_CACHE_SYNC_TASK_OPERATION, STATUS_SCHEDULED);
-  const healingTaskUri = await getTaskUri(HEALING_JOB_OPERATION, HEALING_PATCH_CACHE_GRAPH_TASK_OPERATION, STATUS_SCHEDULED);
 
-  if(syncTaskUri){
-    const task = await loadTask(syncTaskUri);
-    runInitialSyncTask(task);
+  try {
+    //TODO: extra checks are required to make sure the system remains in consistent state
+    // (this responsability might move to background scheduler)
+    //so, e.g. only one scheduled task for jobs of interest, only one job of intersest a time etc..
+    const syncTaskUri = await getTaskUri(INITIAL_CACHE_SYNC_JOB_OPERATION, INITIAL_CACHE_SYNC_TASK_OPERATION, STATUS_SCHEDULED);
+    const healingTaskUri = await getTaskUri(HEALING_JOB_OPERATION, HEALING_PATCH_CACHE_GRAPH_TASK_OPERATION, STATUS_SCHEDULED);
+
+    if(syncTaskUri){
+      const task = await loadTask(syncTaskUri);
+      await runInitialSyncTask(task);
+    }
+    else if(healingTaskUri){
+      const task = await loadTask(healingTaskUri);
+      await runHealingTask(task);
+    }
   }
-  else if(healingTaskUri){
-    const task = await loadTask(syncTaskUri);
-    runHealingTask(task);
+  catch(e){
+    console.error(e);
+    await storeError(e);
   }
 }
 
