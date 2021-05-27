56c792aaedeb930a345c4c05a9607e7fcad24f01
Felix Ruiz de Arcaute <ruiz.felixruiz@gmail.com>
2021-05-27 14:07:06 +0200

    Optimise the export of delta: reduce it to
    effective changes

--- cache-maintenance/main.js
+++ cache-maintenance/main.js
@@ -1,6 +1,8 @@
+import { uuid, sparqlEscapeUri } from 'mu';
+import { querySudo as query, updateSudo as update } from '@lblod/mu-auth-sudo';
 import { produceConceptSchemeDelta } from './producer';
 import { CACHE_GRAPH } from '../env-config';
-import { serializeTriple, storeError, batchedUpdate } from '../lib/utils';
+import { serializeTriple, storeError, batchedUpdate, batchedQuery } from '../lib/utils';
 import { chain } from 'lodash';
 
 //TODO: consider bringing the processing of cache under a job operation.
@@ -8,23 +10,15 @@ import { chain } from 'lodash';
 export async function updateCacheGraph( deltaPayload ){
   try {
     let delta = await produceConceptSchemeDelta(deltaPayload);
-    //TODO: an optimisation step of folding the changesets
-    // + removing redundant inserts/deletes (we have the cache graph we can compare to)
-    // This will be a huge efficiency and ease of debugging win.
-    // However, there is an annoying technical bit in folding, where sometimes
-    // triples coming from deltas are in different lexical space and same logical value.
-    // e.g. 2021-05-04T00:00:00Z vs 2021-05-04T00:00:000Z
-    // Comparing these may not be super straightforward.
-    // Because it's just bugprone to implement, need to look for the right library.
+    const actualchanges = await filterActualChangesToCacheGraph(delta);
 
-    //always first delete then insert
-    const deletes = chain(delta)
+    const deletes = chain(actualchanges)
           .map(c => c.deletes)
           .flatten()
           .map(t => serializeTriple(t))
           .value();
 
-    const inserts = chain(delta)
+    const inserts = chain(actualchanges)
           .map(c => c.inserts)
           .flatten()
           .map(t => serializeTriple(t))
@@ -44,4 +38,123 @@ export async function updateCacheGraph( deltaPayload ){
     console.error(errorMsg);
     await storeError(errorMsg);
   }
+}
+
+async function filterActualChangesToCacheGraph(delta){
+  // We need to fold the changeset to compare the effective wanted changes against the cache graph.
+  // Suppose:
+  //   - delta: [ deletes: s1, insert: s1]
+  //   - targetGraph: { s1 }
+  //   - An effective delete: remove a statement that was in the graph. Vice versa for effective insert.
+  // Comparing the atomic delete from delta directly to the target graph, to conclude it is an effective change yields
+  // wrong results, because the insert:s1 won't be considered an effective insert.
+  // I.e. we end up with delta: [ deletes: s1 ] to execute against the cache graph.
+  const foldedDelta = await foldChangeSet(delta);
+  const foldedDeletes = chain(foldedDelta).map(c => c.deletes).flatten().value();
+  const foldedInserts = chain(foldedDelta).map(c => c.inserts).flatten().value();
+  const actualDeletes = [];
+
+  //From this folded information, we now check wether the cache graph needs an update
+  for(const triple of foldedDeletes){
+    if((await tripleExists(triple, CACHE_GRAPH)) ){
+      actualDeletes.push(triple);
+      await new Promise(r => setTimeout(r, 1000)); //performance consideration
+    }
+  }
+
+  const actualInserts = [];
+
+  for(const triple of foldedInserts){
+    if( !(await tripleExists(triple, CACHE_GRAPH)) ){
+      actualInserts.push(triple);
+      await new Promise(r => setTimeout(r, 1000));
+    }
+  }
+
+  if(!(actualInserts.length || actualDeletes.length)){
+    return [];
+  }
+  else {
+    return [ { deletes: actualDeletes, inserts: actualInserts } ];
+  }
+
+}
+
+async function tripleExists( tripleObject, graph ){
+  const tripleStr = serializeTriple(tripleObject);
+  const existsQuery = `
+    ASK {
+      GRAPH ${sparqlEscapeUri(graph)}{
+        ${tripleStr}
+      }
+    }
+  `;
+
+  const result = await query(existsQuery);
+  return result.boolean;
+}
+
+async function foldChangeSet( delta ){
+  //Note: we don't use utils/diffNTriples because the lexical notation from deltas is not consistent
+  // e.g. 2021-05-04T00:00:00Z vs 2021-05-04T00:00:000Z
+  // Therefore, we use the database, that works in logical equivalents.
+  // Implicit Assumption: we never expect huge (as in 50 000 triples) change sets at once.
+  const deletes = chain(delta).map(c => c.deletes).flatten().value();
+  const inserts = chain(delta).map(c => c.inserts).flatten().value();
+
+  //Folding makes only sense when both deletes and inserts are there, else it is already folded
+  if(!(deletes.length && inserts.length)){
+    return delta;
+  }
+
+  else {
+    const tempDeleteGraph = `http://mu.semte.ch/graphs/delta-producer-cache-maintainer/folding/deletes/${uuid()}`;
+    const tempInsertGraph = `http://mu.semte.ch/graphs/delta-producer-cache-maintainer/folding/inserts/${uuid()}`;
+    try {
+
+      await batchedUpdate(deletes.map(t => serializeTriple(t)), tempDeleteGraph, 'INSERT');
+      await batchedUpdate(inserts.map(t => serializeTriple(t)), tempInsertGraph, 'INSERT');
+
+      const queryForFolding = ( sourceGraph, targetGraph ) => {
+        return `
+        SELECT DISTINCT ?subject ?predicate ?object WHERE {
+            GRAPH ${sparqlEscapeUri(sourceGraph)}{
+              ?subject ?predicate ?object.
+            }
+            FILTER NOT EXISTS {
+            GRAPH ${sparqlEscapeUri(targetGraph)}{
+              ?subject ?predicate ?object.
+            }
+           }
+         }
+        `;
+      };
+
+      const foldedDeletes = await batchedQuery(queryForFolding(tempDeleteGraph, tempInsertGraph), 1000);
+      const foldedInserts = await batchedQuery(queryForFolding(tempInsertGraph, tempDeleteGraph), 1000);
+      return [ { deletes: foldedDeletes, inserts: foldedInserts } ];
+    }
+    finally {
+
+      const cleanUpQuery = (graph) => {
+        return `
+           DELETE {
+               GRAPH ${sparqlEscapeUri(graph)} {
+                 ?s ?p ?o
+               }
+           }
+           WHERE {
+               GRAPH ${sparqlEscapeUri(graph)} {
+                 ?s ?p ?o
+               }
+           }
+        `;
+      };
+
+      await update(cleanUpQuery(tempDeleteGraph));
+      await update(cleanUpQuery(tempInsertGraph));
+    }
+
+  }
+
 }
